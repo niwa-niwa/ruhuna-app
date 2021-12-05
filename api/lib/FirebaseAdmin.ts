56b756acec0afe3d744e0b20e45dfdb334e72540
niwa-niwa <urban.hang274@gmail.com>
2021-12-05 17:18:42 +0900

    rfactored

--- api/lib/FirebaseAdmin.ts
+++ api/lib/FirebaseAdmin.ts
@@ -1,20 +1,12 @@
 import FirebaseAdmin from "firebase-admin";
+import { readFileSync } from "fs";
 import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 import { ErrorObj } from "../types/ErrorObj";
 import { generateErrorObj } from "./generateErrorObj";
 
-const serviceAccount: object = Object.freeze({
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-});
+const serviceAccount = JSON.parse(
+  readFileSync("./ruhuna-dev-firebase-adminsdk-zq7q5-af265266fa.json", "utf-8")
+);
 
 FirebaseAdmin.initializeApp({
   credential: FirebaseAdmin.credential.cert(serviceAccount),
