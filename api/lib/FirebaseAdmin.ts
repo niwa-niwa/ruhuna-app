c8f1698e8a8af72e4de772999ab40f479223d67e
niwa-niwa <urban.hang274@gmail.com>
2021-12-06 22:38:07 +0900

    firebase admin can be configured by json file or .env

--- api/lib/FirebaseAdmin.ts
+++ api/lib/FirebaseAdmin.ts
@@ -1,12 +1,44 @@
 import FirebaseAdmin from "firebase-admin";
-import { readFileSync } from "fs";
+import { readFileSync, existsSync } from "fs";
 import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 import { ErrorObj } from "../types/ErrorObj";
 import { generateErrorObj } from "./generateErrorObj";
 
-const serviceAccount = JSON.parse(
-  readFileSync("./ruhuna-dev-firebase-adminsdk-zq7q5-af265266fa.json", "utf-8")
-);
+let serviceAccount: object = {};
+
+/**
+ *  it try to read firebase admin file
+ */
+if (existsSync(process.env.FIREBASE_ADMIN_FILE_PATH || "")) {
+  try {
+    serviceAccount = JSON.parse(
+      readFileSync(process.env.FIREBASE_ADMIN_FILE_PATH || "", "utf-8")
+    );
+  } catch (e) {
+    console.error(e);
+  }
+} else {
+  console.log("Firebase Admin file is nothing");
+}
+
+/**
+ *  if firebase admin file ware nothing, it would read env variables
+ */
+if (!Object.keys(serviceAccount).length) {
+  serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  };
+}
 
 FirebaseAdmin.initializeApp({
   credential: FirebaseAdmin.credential.cert(serviceAccount),
