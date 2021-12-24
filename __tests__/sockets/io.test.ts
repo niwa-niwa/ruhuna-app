7c7874ef5b2ec8548b58bd4221c1fb9b947e0a98
niwa-niwa <urban.hang274@gmail.com>
2021-12-24 23:41:19 +0900

    refactored a socket-client in test-case

--- __tests__/sockets/io.test.ts
+++ __tests__/sockets/io.test.ts
@@ -3,15 +3,10 @@ import { io as client, Socket } from "socket.io-client";
 import { io } from "../../sockets";
 
 describe("TEST Web Socket io", () => {
+
   const port: string = process.env.PORT || "3000";
-
-  const clientSocket_A: Socket = client(`http://localhost:${port}`, {
-    path: "/sockets",
-    query: {
-      token: "aaa_token",
-    },
-  });
-
+  const uri: string = `http://localhost:${port}`;
+  let clientSocket: Socket;
   let serverSocket: any;
 
   beforeAll(() => {
@@ -23,57 +18,93 @@ describe("TEST Web Socket io", () => {
       io.on("connection", (socket) => {
         serverSocket = socket;
       });
-
       // clientSocket_A.on("connect", done);
     });
   });
 
+  afterEach(()=>{
+    clientSocket.close()
+  })
+
   afterAll(() => {
     io.close();
-    clientSocket_A.close();
   });
 
+
   // test("should work", (done) => {
-  //   clientSocket_A.on("hello", (arg: any) => {
+  //   clientSocket = client(uri, {
+  //     path: "/sockets",
+  //     query: {
+  //       token: "aaa_token",
+  //     },
+  //   });
+
+  //   clientSocket.on("hello", (arg: any) => {
   //     expect(arg).toBe("world");
   //     done();
   //   });
+
   //   serverSocket.emit("hello", "world");
+
   // });
 
   // test("should work (with ack)", (done) => {
+
+  //   clientSocket = client(uri, {
+  //     path: "/sockets",
+  //     query: {
+  //       token: "aaa_token",
+  //     },
+  //   });
+
   //   serverSocket.on("hi", (cb: any) => {
   //     cb("hallo");
   //   });
-  //   clientSocket_A.emit("hi", (arg: any) => {
+
+  //   clientSocket.emit("hi", (arg: any) => {
   //     expect(arg).toBe("hallo");
   //     done();
   //   });
+
   // });
 
+
   test("join room", (done) => {
-    clientSocket_A.on("result_join", (data: any) => {
+
+    clientSocket = client(uri, {
+      path: "/sockets",
+      query: {
+        token: "aaa_token",
+      },
+    });
+
+    clientSocket.on("result_join", (data: any) => {
       expect(data.room).toBe("room_a");
       expect(data.status).toBe(true);
       done();
     });
 
-    clientSocket_A.emit("join", {
+    clientSocket.emit("join", {
       token: "a_token",
       room: "room_a",
       id: "my_id",
     });
+
   });
 
+
   test("not token user should be rejected", (done) => {
-    const clientSocket_B: Socket = client(`http://localhost:${port}`, {
+
+    const clientSocket: Socket = client(`http://localhost:${port}`, {
       path: "/sockets",
     });
 
-    clientSocket_B.on("connect_error", (err: any) => {
-      console.log("connect was rejected", err.message);
+    clientSocket.on("connect_error", (err: any) => {
       expect(err.message).toBe("Unauthorized");
       done();
     });
+
   });
+
+
 });
--- sockets/index.ts
+++ sockets/index.ts
@@ -1,5 +1,3 @@
-import { NextFunction } from "express";
-import { NextServer } from "next/dist/server/next";
 import { Server, Socket } from "socket.io";
 
 // TODO create const file to implement paths
@@ -14,11 +12,9 @@ const io: Server = new Server({
 
 io.use((socket: Socket, next) => {
   // TODO verify firebase token
-  console.log("message from middleware");
   const token = socket.handshake.query.token;
 
   if (!token) {
-    console.log("middleware error");
     const err: any = new Error("not authorized");
     err.data = { content: "Please retry later" }; // additional details
     next(new Error("Unauthorized"));
@@ -28,14 +24,12 @@ io.use((socket: Socket, next) => {
 });
 
 io.on("connection", (socket: Socket) => {
-  console.log("a user connected to socket by socketId=" + socket.id);
 
   // TODO create an type of the variable for member of a room {socketId, firebaseToken}
   let currentMembers: object;
 
   // TODO join the room
   socket.on("join", (data) => {
-    console.log("join the room", data.room);
 
     // TODO notify users who are login
     // TODO send status of whether the user can join at the room
