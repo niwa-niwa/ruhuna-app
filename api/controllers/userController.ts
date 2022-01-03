88d0f3b73e28814f538259e5e7eb1c89e70b1f77
niwa-niwa <urban.hang274@gmail.com>
2022-01-03 22:14:03 +0900

    refactored userController

--- api/controllers/userController.ts
+++ api/controllers/userController.ts
@@ -5,37 +5,68 @@ import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 import { prismaClient } from "../../lib/prismaClient";
 import { generateErrorObj } from "../../lib/generateErrorObj";
 import { ErrorObj } from "../types/ErrorObj";
+import { CustomRequest } from "../types/CustomRequest";
 
-export const getUserDetail = async (req: Request, res: Response) => {
+/**
+ * Get user profile detail
+ * @param req
+ * @param res
+ */
+async function getUserDetail(req: Request, res: Response): Promise<void> {
+  // get user id from params
   const id: string = req.params.userId;
 
+  // get model of the user by user id
   const user: User | null = await prismaClient.user.findUnique({
     where: { id },
   });
 
-  if (user) {
-    res.status(200).json({ user });
-  } else {
+  // throw an error if user is null
+  if (!user) {
     res.status(404).json({
       user: null,
       errorObj: generateErrorObj(404, "The User is not Found"),
     });
+    return;
   }
-};
 
-export const getUsers = async (req: Request, res: Response) => {
+  // response the user
+  res.status(200).json({ user });
+
+  return;
+}
+
+/**
+ * Get all users
+ * @param req
+ * @param res
+ */
+async function getUsers(req: Request, res: Response): Promise<void> {
+  // get all users
   const users: User[] = await prismaClient.user.findMany();
-  res.status(200).json({ users: users });
-};
 
-export const createUser = async (req: Request, res: Response) => {
+  // response all user data
+  res.status(200).json({ users: users });
+
+  return;
+}
+
+/**
+ * Create user with firebase token
+ * @param req
+ * @param res
+ */
+async function createUser(req: Request, res: Response): Promise<void> {
+  // get firebase token from body
   const firebaseToken: string = req.body.firebaseToken;
 
   try {
+    // get firebase user from firebase
     const currentUser: DecodedIdToken | ErrorObj = await verifyToken(
       firebaseToken
     );
 
+    // throw an error if currentUser has an errorCode property
     if ("errorCode" in currentUser) {
       // if the token were not authorized, it response error
       res.status(currentUser.errorCode).json({
@@ -46,14 +77,18 @@ export const createUser = async (req: Request, res: Response) => {
     }
 
     if ("uid" in currentUser) {
-      const data: Prisma.UserCreateInput = {
-        firebaseId: currentUser.uid,
-        username: currentUser.name,
-      };
+      // create user
+      const createdUser: User = await prismaClient.user.create({
+        data: {
+          firebaseId: currentUser.uid,
+          username: currentUser.name,
+        },
+      });
 
-      const createdUser: User = await prismaClient.user.create({ data });
-
+      // response created user data
       res.status(200).json({ user: createdUser });
+
+      return;
     }
   } catch (e) {
     console.error(e);
@@ -62,11 +97,22 @@ export const createUser = async (req: Request, res: Response) => {
       .status(404)
       .json({ errorObj: generateErrorObj(404, "Couldn't create a user") });
   }
-};
+}
 
-export const editUser = async (req: Request, res: Response) => {
+/**
+ * edit a user with currentUser.id who sent request
+ * @param req
+ * @param res
+ */
+async function editUser(req: CustomRequest, res: Response): Promise<void> {
   try {
-    const id: string = req.params.userId;
+    let id: string | undefined = req.currentUser?.id;
+
+    // if the user who sent request is admin it would confirm params.userId
+    if (req.currentUser?.isAdmin) {
+      id = req.params.userId || req.currentUser?.id;
+    }
+
     const data: Prisma.UserUpdateInput = req.body;
 
     const editedUser: User = await prismaClient.user.update({
@@ -75,25 +121,38 @@ export const editUser = async (req: Request, res: Response) => {
     });
 
     res.status(200).json({ user: editedUser });
+
+    return;
   } catch (e) {
     console.error(e);
-
     res.status(404).json({
       user: null,
       errorObj: generateErrorObj(404, "The User is not Found"),
     });
   }
-};
+}
 
-export const deleteUser = async (req: Request, res: Response) => {
+/**
+ * delete a user with currentUser.id who sent request
+ * @param req
+ * @param res
+ */
+async function deleteUser(req: CustomRequest, res: Response): Promise<void> {
   try {
-    const id: string = req.params.userId;
+    let id: string | undefined = req.currentUser?.id;
+
+    // if the user who sent request is admin it would confirm params.userId
+    if (req.currentUser?.isAdmin) {
+      id = req.params.userId || req.currentUser?.id;
+    }
 
     const deletedUser: User = await prismaClient.user.delete({
       where: { id },
     });
 
     res.status(200).json({ user: deletedUser });
+
+    return;
   } catch (e) {
     console.error(e);
     res.status(404).json({
@@ -101,9 +160,15 @@ export const deleteUser = async (req: Request, res: Response) => {
       errorObj: generateErrorObj(404, "the user is not found"),
     });
   }
-};
+}
 
-const userController = {
+const userController: {
+  getUsers: any;
+  getUserDetail: any;
+  createUser: any;
+  editUser: any;
+  deleteUser: any;
+} = {
   getUsers,
   getUserDetail,
   createUser,
