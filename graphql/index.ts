a5942f56b34aa237fcc1ed27a5f338c00db228d3
niwa-niwa <urban.hang274@gmail.com>
2022-02-11 01:30:43 +0900

    installed packages for merging each schemas and resolvers files

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,21 +1,34 @@
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { loadSchemaSync } from "@graphql-tools/load";
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { addResolversToSchema } from "@graphql-tools/schema";
 import { ApolloServer } from "apollo-server-express";
 import { ExpressContext } from "apollo-server-express";
 import { join } from "path";
-import { resolvers } from './resolvers'
+import { resolvers } from "./resolvers/con.resolvers";
 
-const schema = loadSchemaSync(join(__dirname, "/schema.graphql"), {
-  loaders: [new GraphQLFileLoader()],
-});
+// const schema = loadSchemaSync(join(__dirname, "/schema.graphql"), {
+//   loaders: [new GraphQLFileLoader()],
+// });
 
-const schemaWithResolvers = addResolversToSchema({
-  schema,
-  resolvers,
-});
+// const schemaWithResolvers = addResolversToSchema({
+//   schema:mergeTypeDefs(schema),
+//   resolvers,
+// });
+
+// merge types
+const typesArray: any[] = loadFilesSync(
+  join(__dirname, "/schemas/**/*.graphql")
+);
+
+// merge resolvers
+const resolversArray: any[] = loadFilesSync(
+  join(__dirname, "/resolvers/**/*.resolvers.*")
+);
 
 export const apolloServer: ApolloServer<ExpressContext> = new ApolloServer({
-  schema: schemaWithResolvers,
+  typeDefs: mergeTypeDefs(typesArray),
+  resolvers: mergeResolvers(resolversArray),
   context: ({ req }) => {},
 });
