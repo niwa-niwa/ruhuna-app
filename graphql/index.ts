986dfb49e580d266e3dea4fb74a616bf9368c663
niwa-niwa <urban.hang274@gmail.com>
2022-02-12 13:47:03 +0900

    init codegen for generating types and types of resolvers

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,21 +1,8 @@
-import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
-import { loadSchemaSync } from "@graphql-tools/load";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
-import { addResolversToSchema } from "@graphql-tools/schema";
 import { ApolloServer } from "apollo-server-express";
 import { ExpressContext } from "apollo-server-express";
 import { join } from "path";
-import { resolvers } from "./resolvers/con.resolvers";
-
-// const schema = loadSchemaSync(join(__dirname, "/schema.graphql"), {
-//   loaders: [new GraphQLFileLoader()],
-// });
-
-// const schemaWithResolvers = addResolversToSchema({
-//   schema:mergeTypeDefs(schema),
-//   resolvers,
-// });
 
 // merge types
 const typesArray: any[] = loadFilesSync(
