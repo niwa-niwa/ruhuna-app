d96cfe1148e6507e4cd0b8ad019300db7c6db28b
niwa-niwa <urban.hang274@gmail.com>
2022-02-18 18:27:33 +0900

    implemented Querys for user

--- graphql/resolvers/User.resolvers.ts
+++ graphql/resolvers/User.resolvers.ts
@@ -1,7 +1,32 @@
+import { prismaClient } from "../../lib/prismaClient";
+import { User } from "@prisma/client";
+
 export const resolvers = {
   Query: {
-    getMe: (parent: any, args: any, context: any, info: any) => {
+    getMe: (parent: any, args: any, context: any, info: any): User => {
       return context.currentUser;
+    },
+    getUsers: async (): Promise<User[]> => {
+      // TODO include messages and villages
+      const users: User[] = await prismaClient.user.findMany();
+      return users;
+    },
+    getUserDetail: async (
+      parent: any,
+      { id }: { id: string },
+      context: any,
+      info: any
+    ): Promise<User> => {
+      // TODO include messages and villages
+      const user: User | null = await prismaClient.user.findUnique({
+        where: { id },
+      });
+      // throw an error if user is null
+      if (!user) {
+        throw new Error("bad parameter request");
+      }
+
+      return user;
     },
   },
 };
