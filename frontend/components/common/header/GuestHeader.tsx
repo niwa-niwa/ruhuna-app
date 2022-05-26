15eaead057aee8805fee4822d80bfeb2ecba319e
niwa-niwa <urban.hang274@gmail.com>
2022-05-27 00:43:42 +0900

    implemented guest header

--- frontend/components/common/header/GuestHeader.tsx
+++ frontend/components/common/header/GuestHeader.tsx
@@ -1,21 +1,76 @@
-import type { NextPage } from "next";
-import { JP } from "../../../../consts/texts";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 import { css } from "@emotion/react";
-import { CenteringDiv, centering } from "../../../styles";
+import { JP } from "../../../../consts/texts";
 
-// TODO implement a header for guest
-const GuestHeader: NextPage = () => {
+export default function GuestHeader() {
+  const pages = ["Sign In", "Sign Up"];
+
   return (
-    <CenteringDiv>
-      <h2
+    <AppBar position="static">
+      <Container
         css={css`
-          margin: 0;
+          display: flex;
+          flex-flow: column;
+          align-items: center;
         `}
       >
-        {JP.app_name}
-      </h2>
-      <a href="/a">{JP.st_create_account}</a>
-    </CenteringDiv>
+        <Container
+          maxWidth="xl"
+          css={css`
+            display: contents;
+          `}
+        >
+          <Typography
+            variant="h1"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              mt: 1.5,
+              display: { xs: "flex" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+              fontSize: 32,
+            }}
+          >
+            {JP.app_name}
+          </Typography>
+        </Container>
+
+        <Container
+          maxWidth="xl"
+          color="secondary"
+          css={css`
+            display: contents;
+          `}
+        >
+          <Box sx={{ flexGrow: 1, display: { md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{
+                  my: 0,
+                  mx: 5,
+                  color: "white",
+                  display: "inline",
+                  fontSize: 16,
+                }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Container>
+      </Container>
+    </AppBar>
   );
-};
-export default GuestHeader;
+}
