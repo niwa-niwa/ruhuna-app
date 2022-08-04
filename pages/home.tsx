3ed518740eb30441a9d96ef577e1948952d8e5eb
niwa-niwa <urban.hang274@gmail.com>
2022-08-04 23:57:06 +0900

    created Home body

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,59 +1,23 @@
-import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { Button } from "@mui/material";
-import { useEffect, useState } from "react";
-import { client_auth } from "../frontend/lib/firebaseApp";
-import Router from "next/router";
 import AuthLayout from "../frontend/components/common/layout/AuthLayout";
-
-const initFirebaseAuth = () => {
-  return new Promise((resolve) => {
-    var unsubscribe = client_auth.onAuthStateChanged((user) => {
-      // user オブジェクトを resolve
-      resolve(user);
-
-      // 登録解除
-      unsubscribe();
-    });
-  });
-};
+import HomeBody from "../frontend/components/pages/HomeBody";
+import { useLocale, LocaleText } from "../frontend/hooks/useLocal";
 
 const Home = () => {
-  // const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  // useEffect(() => {
-  //   console.log("Home CurrentUser = ", client_auth.currentUser);
-
-  //   if (typeof window !== "undefined") {
-  //     if (!client_auth.currentUser) {
-  //       const unsubscribe = client_auth.onAuthStateChanged((user) => {
-  //         unsubscribe();
-          
-  //         if (!user) {
-  //           Router.push("/");
-  //           return;
-  //         }
-  //         console.log("shcke",user)
-
-  //         setIsLoading(false);
-  //       });
-  //     }
-
-  //     setIsLoading(false);
-  //   }
-  // }, []);
-
-  // if(isLoading){
-  //   return <h1>Loading...</h1>
-  // }
+  const { txt }: { txt: LocaleText } = useLocale();
 
   return (
     <AuthLayout>
-      <h1>ホーム画面</h1>
+      <Head>
+        <title>
+          {txt.signin} | {txt.app_name}
+        </title>
+        <meta name="description" content={txt.app_name + txt.signin} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HomeBody />
     </AuthLayout>
-  )
+  );
 };
 
 export default Home;
